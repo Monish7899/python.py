a98e3b84bf4349df0d524962632e81cafe84f389
Monish7899 <231001120@rajalakshmi.edu.in>
2024-06-15 20:49:48 +0530

    Create week 6.2.py

---
+++ week 6/week 6.2.py
@@ -0,0 +1,13 @@
+Assume that the given string has enough memory. Don't use any extra space(INPLACE)
+Sample Input 1
+a2b4c6
+Sample Output 1
+aabbbbcccccc
+import re
+a=input()
+all=re.findall('\d+',a)
+all_w=re.findall('[a-z]',a)
+b=''
+for i,j in zip(all,all_w):
+ b+=int(i)*j
+print(b)
